cdf9d70c50584e63c440da6785152ccd9e105289
Adriel Ricardo <adrieldesigner@hotmail.com>
2019-06-15 10:37:44 -0300

    Versão 1.0.0a

---
+++ 3P_tb003_TemaLivre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3P_tb003_TemaLivre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo!");
+        }
+    }
+}
