e67311889bd86da00df92a88e3d618fd2db773ac
Eder Maciel <456eder@gmail.com>
2019-06-16 17:10:10 -0300

    O método de registrar,entraria nessa classe também,né?

---
+++ 3P_tb003_TemaLivre/Cachorro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3P_tb003_TemaLivre
+{
+    class Cachorro
+    {
+        private string raca;
+        private double peso;
+
+        public string Raca { get { return raca; } set { raca = value; } }
+        public double Peso { get { return peso; } set { peso = value; } }
+
+        public Cachorro() { }//AQUI INICIALIZA A LISTA TAMBÉM ?
+
+    }
+}
