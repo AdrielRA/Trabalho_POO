1b95db6a172652dd914d4b7f99e93a795f46343c
AdrielRA <adrieldeveloper@hotmail.com>
2019-06-17 13:20:51 -0300

    Versão 1.0.3

--- 3P_tb003_TemaLivre/Cachorro.cs
+++ 3P_tb003_TemaLivre/Cachorro.cs
@@ -7,16 +7,15 @@ namespace _3P_tb003_TemaLivre
     class Cachorro:Animal
     {
         private TCor cor_Pelo;
-        private string comportamento_cachorro;
-        private int expectativa;
 
         public string Cor_Pelo { set { cor_Pelo.Nome_cor = value; } }
-        public string Comportamento { set { comportamento_cachorro = value; } }
-        public int Expectativa { set { expectativa = value; } }
+        public string Comportamento { set { comportamento = value; } }
+        public int Expectativa { set { expectativa_vida = value; } }
 
         public Cachorro(double peso,double altura):base(peso,altura)
         {
-           
+            cor_Pelo = new TCor();
+            especie = "Cachorro";
         }
 
         override public string Falar()
@@ -26,7 +25,10 @@ namespace _3P_tb003_TemaLivre
         }
        override public string Mostra_Caracteristicas()
         {
-            return base.Mostra_Caracteristicas()+"Caracteristicas do cão :\n"+"Cor do Pelo :"+ cor_Pelo.Nome_cor +"Som :" +Falar();
+            return base.Mostra_Caracteristicas()+ "\n" + 
+                   "Caracteristicas do cão:\n" +
+                   "Cor do Pelo: " + cor_Pelo.Nome_cor + "\n" +
+                   "Fala: " + Falar();
         }
     }
 }
--- 3P_tb003_TemaLivre/Program.cs
+++ 3P_tb003_TemaLivre/Program.cs
@@ -9,7 +9,7 @@ namespace _3P_tb003_TemaLivre
         static void Main(string[] args)
         {
             List<Animal> List_Animais = new List<Animal>();
-            Gato gato = null;
+            //Gato gato = null;
             Cachorro cachorro = null;
             int op = 0;
             do
@@ -23,7 +23,7 @@ namespace _3P_tb003_TemaLivre
                 Console.WriteLine("[ 3 ] Consultar caracteristicas");
                 Console.WriteLine("[ 4 ] Sair");
                 Console.WriteLine("____________________________________");
-                Console.WriteLine("Sua opção: ");
+                Console.Write("Sua opção: ");
                 op = int.Parse(Console.ReadLine());
                 Console.Clear();
                 switch (op)
@@ -50,34 +50,34 @@ namespace _3P_tb003_TemaLivre
                         List_Animais.Add(cachorro);
                         break;
                     case 2:
-                        Console.WriteLine("____________________________________");
-                        Console.WriteLine(">>> CADASTRO: Gatos");
-                        Console.WriteLine("____________________________________");
-                        Console.Write("Nome: ");
-                        string nome = Console.ReadLine();
-                        double peso, altura;
-                        Console.Write("Peso: ");
-                        peso = double.Parse(Console.ReadLine());
-                        Console.Write("Altura: ");
-                        altura = double.Parse(Console.ReadLine());
-                        gato = new Gato(peso, altura);
-                        gato.nome = nome;
-                        Console.Write("Expectativa de vida: ");
-                        gato.Expectativa = int.Parse(Console.ReadLine());
-                        Console.Write("Comportamento: ");
-                        gato.Comportamento = Console.ReadLine();
-                        Console.Write("Cor dos olhos: ");
-                        gato.Cor_Olhos = Console.ReadLine();
-                        Console.Write("Tipo de unha: ");
-                        gato.Unhas = Console.ReadLine();
-                        List_Animais.Add(gato);
+                        //Console.WriteLine("____________________________________");
+                        //Console.WriteLine(">>> CADASTRO: Gatos");
+                        //Console.WriteLine("____________________________________");
+                        //Console.Write("Nome: ");
+                        //string nome = Console.ReadLine();
+                        //double peso, altura;
+                        //Console.Write("Peso: ");
+                        //peso = double.Parse(Console.ReadLine());
+                        //Console.Write("Altura: ");
+                        //altura = double.Parse(Console.ReadLine());
+                        //gato = new Gato(peso, altura);
+                        //gato.nome = nome;
+                        //Console.Write("Expectativa de vida: ");
+                        //gato.Expectativa = int.Parse(Console.ReadLine());
+                        //Console.Write("Comportamento: ");
+                        //gato.Comportamento = Console.ReadLine();
+                        //Console.Write("Cor dos olhos: ");
+                        //gato.Cor_Olhos = Console.ReadLine();
+                        //Console.Write("Tipo de unha: ");
+                        //gato.Unhas = Console.ReadLine();
+                        //List_Animais.Add(gato);
                         break;
                     case 3:
                         Console.WriteLine("____________________________________");
                         Console.WriteLine(">>> CONSULTA");
                         Console.WriteLine("____________________________________");
                         Consultar(List_Animais);
-                        Console.WriteLine(">>> VOLTAR...");
+                        Console.Write(">>> VOLTAR...");
                         Console.ReadKey();
                         break;
                     default:
