7813eb94cc9ce1750a4831cc4df6151cb96b2a23
AdrielRA <adrieldeveloper@hotmail.com>
2019-06-17 16:21:41 -0300

    Versão 1.0.5

--- 3P_tb003_TemaLivre/Program.cs
+++ 3P_tb003_TemaLivre/Program.cs
@@ -9,8 +9,10 @@ namespace _3P_tb003_TemaLivre
         static void Main(string[] args)
         {
             List<Animal> List_Animais = new List<Animal>();
-            //Gato gato = null;
+            Gato gato = null;
             Cachorro cachorro = null;
+            double peso, altura;
+            string nome;
             int op = 0;
             do
             {
@@ -20,8 +22,9 @@ namespace _3P_tb003_TemaLivre
                 Console.WriteLine("____________________________________");
                 Console.WriteLine("[ 1 ] Cadastrar cães");
                 Console.WriteLine("[ 2 ] Cadastrar gatos");
-                Console.WriteLine("[ 3 ] Consultar caracteristicas");
-                Console.WriteLine("[ 4 ] Sair");
+                Console.WriteLine("[ 3 ] Consultar por nome");
+                Console.WriteLine("[ 4 ] Consultar todos");
+                Console.WriteLine("[ 5 ] Sair");
                 Console.WriteLine("____________________________________");
                 Console.Write("Sua opção: ");
                 op = int.Parse(Console.ReadLine());
@@ -33,8 +36,7 @@ namespace _3P_tb003_TemaLivre
                         Console.WriteLine(">>> CADASTRO: Cães");
                         Console.WriteLine("____________________________________");
                         Console.Write("Nome: ");
-                        string nome = Console.ReadLine();
-                        double peso, altura;
+                        nome = Console.ReadLine();
                         Console.Write("Peso: ");
                         peso = double.Parse(Console.ReadLine());
                         Console.Write("Altura: ");
@@ -50,50 +52,69 @@ namespace _3P_tb003_TemaLivre
                         List_Animais.Add(cachorro);
                         break;
                     case 2:
-                        //Console.WriteLine("____________________________________");
-                        //Console.WriteLine(">>> CADASTRO: Gatos");
-                        //Console.WriteLine("____________________________________");
-                        //Console.Write("Nome: ");
-                        //string nome = Console.ReadLine();
-                        //double peso, altura;
-                        //Console.Write("Peso: ");
-                        //peso = double.Parse(Console.ReadLine());
-                        //Console.Write("Altura: ");
-                        //altura = double.Parse(Console.ReadLine());
-                        //gato = new Gato(peso, altura);
-                        //gato.nome = nome;
-                        //Console.Write("Expectativa de vida: ");
-                        //gato.Expectativa = int.Parse(Console.ReadLine());
-                        //Console.Write("Comportamento: ");
-                        //gato.Comportamento = Console.ReadLine();
-                        //Console.Write("Cor dos olhos: ");
-                        //gato.Cor_Olhos = Console.ReadLine();
-                        //Console.Write("Tipo de unha: ");
-                        //gato.Unhas = Console.ReadLine();
-                        //List_Animais.Add(gato);
+                        Console.WriteLine("____________________________________");
+                        Console.WriteLine(">>> CADASTRO: Gatos");
+                        Console.WriteLine("____________________________________");
+                        Console.Write("Nome: ");
+                        nome = Console.ReadLine();
+                        Console.Write("Peso: ");
+                        peso = double.Parse(Console.ReadLine());
+                        Console.Write("Altura: ");
+                        altura = double.Parse(Console.ReadLine());
+                        gato = new Gato(peso, altura);
+                        gato.nome = nome;
+                        Console.Write("Expectativa de vida: ");
+                        gato.Expectativa = int.Parse(Console.ReadLine());
+                        Console.Write("Comportamento: ");
+                        gato.Comportamento = Console.ReadLine();
+                        Console.Write("Cor dos olhos: ");
+                        gato.Cor_Olhos = Console.ReadLine();
+                        Console.Write("Tipo de unha: ");
+                        gato.Unhas = Console.ReadLine();
+                        List_Animais.Add(gato);
                         break;
                     case 3:
                         Console.WriteLine("____________________________________");
-                        Console.WriteLine(">>> CONSULTA");
+                        Console.WriteLine(">>> CONSULTA: Nome");
                         Console.WriteLine("____________________________________");
-                        Consultar(List_Animais);
+                        Console.Write("Nome do animal: ");
+                        nome = Console.ReadLine();
+                        Console.WriteLine("____________________________________");
+                        bool achou = false;
+                        foreach (Animal a in List_Animais)
+                        {
+                            if (a.nome == nome)
+                            {
+                                Console.WriteLine(a.Mostra_Caracteristicas());
+                                Console.WriteLine("____________________________________");
+                                achou = true;
+                                break;
+                            }
+                        }
+                        if (!achou)
+                        {
+                            Console.WriteLine("Animal não encontrado!");
+                            Console.WriteLine("____________________________________");
+                        }
+                        Console.Write(">>> VOLTAR...");
+                        Console.ReadKey();
+                        break;
+                    case 4:
+                        Console.WriteLine("____________________________________");
+                        Console.WriteLine(">>> CONSULTA: Todos");
+                        Console.WriteLine("____________________________________");
+                        foreach (Animal a in List_Animais)
+                        {
+                            Console.WriteLine(a.Mostra_Caracteristicas());
+                            Console.WriteLine("____________________________________");
+                        }
                         Console.Write(">>> VOLTAR...");
                         Console.ReadKey();
                         break;
                     default:
                         break;
                 }
-
-            } while (op != 4);
-        }
-
-        static void Consultar(List<Animal> lista)
-        {
-            foreach (Animal a in lista)
-            {
-                Console.WriteLine(a.Mostra_Caracteristicas());
-                Console.WriteLine("____________________________________");
-            }
+            } while (op != 5);
         }
     }
 }
